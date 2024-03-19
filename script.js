71b3d174ba307bbf8cc414b5351fc28c73efd5f2
Joel Victor <146671323+joelvictordev@users.noreply.github.com>
2024-03-19 15:42:08 -0300

    Atualização do Javascript

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ var outInput = document.querySelector("#resultado");
 function criptografar() {
     var texto = textInput.value;
 
-    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
-    document.getElementById('resultado').innerHTML = '<textarea class="resultadoscript" readonly id="input-texto">' + resultCripto + '</textarea>' + '<button class="botoes__copiar" id="copiar" onclick="copiar()">Copiar</button>';
+    document.getElementById('resultado').innerHTML = '<textarea class="resultadoscript" readonly id="input-texto">' + resultCripto + '</textarea>' + '<button class="botoes__copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function descriptografar() {
     var texto = textInput.value;
 
-    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u")
 
-    document.getElementById('resultado').innerHTML = '<textarea class="resultadoscript" readonly id="input-texto">' + resultDescripto + '</textarea>' + '<button class="botoes__copiar" id="copiar" onclick="copiar()">Copiar</button>';
+    document.getElementById('resultado').innerHTML = '<textarea class="resultadoscript" readonly id="input-texto">' + resultDescripto + '</textarea>' + '<button class="botoes__copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
 function copiar() {
